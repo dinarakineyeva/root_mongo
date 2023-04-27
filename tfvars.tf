4625d3b1d1d7deb5194b89296c7ddf25da8d0f06
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-04-27 11:56:09 -0400

    Create tfvars.tf

---
+++ tfvars.tf
@@ -0,0 +1,30 @@
+######################################
+########## MONGO DB ATLAS ############
+######################################
+gcp_project_id = ""
+mongo_project_id      = ""
+mongo_public_key      = ""
+mongo_private_key     = ""
+gcp_region            = ""
+cloud_provider        = "GCP"
+mongo_cluster_name    = "test-cluster-m"
+mongo_cluster_version = "4.4"
+mongo_cluster_type    = "REPLICASET"
+cloud_backup          = true
+autoscaling           = true
+mongo_cluster_size    = "M10"
+num_shards            = 1
+mongo_cluster_region  = ""
+electable_nodes       = 3
+priority              = 7
+read_only_nodes       = 0
+subnet                = ""
+db_username           = "user"
+db_password           = "user"
+auth_database_name    = "admin"
+db_role_name          = "readWrite"
+database_name         = "dbforapp"
+db_key                = "key"
+db_value              = "value"
+google_compute_address = "10.142.0."
+google_compute_address_type = "INTERNAL"

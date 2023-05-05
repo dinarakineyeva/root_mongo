843d96a90b1776663a37624930abb15f1e61dfbf
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-05-05 13:49:32 -0400

    Update tfvars.tf

--- tfvars.tf
+++ tfvars.tf
@@ -1,29 +1,29 @@
 ######################################
 ########## MONGO DB ATLAS ############
 ######################################
-gcp_project_id = ""
-mongo_project_id      = ""
-mongo_public_key      = ""
-mongo_private_key     = ""
-gcp_region            = ""
+gcp_project_id = "elaborate-hash-385117"
+mongo_project_id      = "644fa96ebff4ee115f6d7642"
+mongo_public_key      = "snjevltv"
+mongo_private_key     = "a10b55da-105f-4f02-97ed-9349baf17668"
+gcp_region            = "us-central1"
 cloud_provider        = "GCP"
 mongo_cluster_name    = "test-cluster-m"
 mongo_cluster_version = "4.4"
 mongo_cluster_type    = "REPLICASET"
 cloud_backup          = true
 autoscaling           = true
-mongo_cluster_size    = "M10"
+mongo_cluster_size    = "M40"
 num_shards            = 1
-mongo_cluster_region  = ""
+mongo_cluster_region  = "CENTRAL_US"
 electable_nodes       = 3
 priority              = 7
 read_only_nodes       = 0
-subnet                = ""
+subnet                = "us-central1/default"
 db_username           = "user"
 db_password           = "user"
 auth_database_name    = "admin"
-db_role_name          = "readWrite"
-database_name         = "dbforapp"
+db_role_name          = "readWriteAnyDatabase"
+database_name         = "admin"
 db_key                = "key"
 db_value              = "value"
 google_compute_address = "10.142.0."

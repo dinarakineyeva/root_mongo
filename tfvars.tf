3a24cbc0e0046091d20e63f1613ea92c4d54d704
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-07 11:20:47 -0400

    Update tfvars.tf

--- tfvars.tf
+++ tfvars.tf
@@ -1,30 +1,38 @@
 ######################################
 ########## MONGO DB ATLAS ############
 ######################################
-gcp_project_id = "elaborate-hash-385117"
-mongo_project_id      = "644fa96ebff4ee115f6d7642"
-mongo_public_key      = "snjevltv"
-mongo_private_key     = "a10b55da-105f-4f02-97ed-9349baf17668"
+gcp_project_id = "triple-acre-388315"
+mongo_project_id      = "64779263a0390527428ccb1f"
+mongo_public_key      = "vwruubsz"
+mongo_private_key     = "731ddc21-e4b9-4fb3-a93e-9341ff08efa2"
 gcp_region            = "us-central1"
 cloud_provider        = "GCP"
 mongo_cluster_name    = "test-cluster-m"
-mongo_cluster_version = "4.4"
+mongo_cluster_version = "6.0"
 mongo_cluster_type    = "REPLICASET"
 cloud_backup          = true
 autoscaling           = true
-mongo_cluster_size    = "M40"
+termination_protection_enabled = false
+advanced_configuration = {
+  javascript_enabled = true
+  minimum_enabled_tls_protocol = "TLS1_2"
+  oplog_min_retention_hours = 24
+}
+mongo_cluster_size    = "M10"
 num_shards            = 1
 mongo_cluster_region  = "CENTRAL_US"
 electable_nodes       = 3
 priority              = 7
 read_only_nodes       = 0
-subnet                = "us-central1/default"
-db_username           = "user"
+db_username           = "devuser"
 db_password           = "user"
 auth_database_name    = "admin"
 db_role_name          = "readWriteAnyDatabase"
 database_name         = "admin"
 db_key                = "key"
 db_value              = "value"
-google_compute_address = "10.142.0."
+google_compute_address = "10.0.42."
 google_compute_address_type = "INTERNAL"
+network_name = "dev-newtork"
+subnet_name = "dev-subnet"
+compute_address_name = "dev-mongo"

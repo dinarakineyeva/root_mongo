15b75f227e575645a625537acad618d0e5ef18ef
dinarakineyeva <79667897+dinarakineyeva@users.noreply.github.com>
2023-06-28 07:59:52 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,23 @@
-output "db_user" {
+output "user1" {
   value = module.atlas_cluster.user1
 }
 
-output "database-rw-user" {
-  value = module.atlas_cluster.database-rw-user
+output "database_ro_username" {
+  value = module.atlas_cluster.database_ro_username
 }
 
-output "database-admin-user" {
-  value = module.atlas_cluster.database-admin-user
+output "database_rw_username" {
+  value = module.atlas_cluster.database_rw_username
 }
 
-output "name" {
-  value = module.atlas_cluster.connection_string
+output "database_admin_username" {
+  value = module.atlas_cluster.database_admin_username
+}
+
+output "cluster_connection_strings" {
+  value = module.atlas_cluster.cluster_connection_strings
+}
+
+output "mongo_project_id" {
+  value = module.atlas_cluster.mongo_project_id
 }
